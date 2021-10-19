a379ea5cd21e9612ed025c5a664d4df7d3beaac0
ItachiUchiha504 <eduardowillingham@gmail.com>
2021-10-19 17:20:02 -0600

    new update

---
+++ script.js
@@ -0,0 +1,49 @@
+const zero = document.getElementById("zero")
+const one = document.getElementById("one")
+const two = document.getElementById("two")
+const three = document.getElementById("three")
+const four = document.getElementById("four")
+const five = document.getElementById("five")
+const six = document.getElementById("six")
+const seven = document.getElementById("sevev")
+const eight = document.getElementById("eight")
+const nine = document.getElementById("nine")
+const addition = document.getElementById("plus")
+const subtraction = document.getElementById("mins")
+const division = document.getElementById("divide")
+const remainder = document.getElementById("remainder")
+const AC = document.getElementById("refresh")
+const times = document.getElementById("times")
+const equal = document.getElementById("equal")
+const decimal = document.getElementById("decimal")
+const backspace = document.getElementById("delete")
+let result = document.getElementById("result");
+let userScore = 0; 
+
+
+
+    function sum(){
+        for(i=0;i<=50;i++){
+            zero.addEventListener("click",()=>{
+                return 0;
+            })
+
+        } break (equal.addEventListener("click",()=>{
+            return answer
+        }))
+
+
+
+}
+
+equal.addEventListener("click", ()=>{
+  result.innerHTML = userScore++;
+})
+
+one.addEventListener("click", ()=>{
+    result.innerHTML = userScore--;
+  })
+
+
+
+
